b4366955cf9ef1bc483a31af23072a35ddbe2d4b
LENOVO <LENOVO@DESKTOP-K0S4J5J>
2023-11-27 12:42:19 +0330

    Add project files.

---
+++ test4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace test4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("salam donyaaaaaaaaaa");
+        }
+    }
+}
